d25a98c2b93322b8d4f7ed71bb8f43830cd76450
Dominic Ong <dominic.ong.89@gmail.com>
2019-02-09 03:21:22 +0800

    Updates

---
+++ pages-manifest-713a036d1560cb2522c4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{139:function(n){n.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"},{componentChunkName:"component---src-pages-nav-js",jsonName:"nav-ce4",path:"/nav/"}],dataPaths:{index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E","nav-ce4":"122/path---nav-ce-4-92c-NZuapzHg3X9TaN1iIixfv1W23E"}}}}]);
+//# sourceMappingURL=pages-manifest-713a036d1560cb2522c4.js.map
